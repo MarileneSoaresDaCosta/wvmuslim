461f667e5d205ada77581140cd125957b6e6145b
Marilene <lene626@gmail.com>
2018-02-13 21:58:14 -0800

    Added carousel and prayer times

---
+++ index2.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>WVMA (mock grid flex)</title>
+
+
+<!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  
+  <link rel="icon" href="resources/arabesqueIcon2Black.png">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="wvmuslim.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+
+<!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400,400i,700,900|Open+Sans:300,300i,400,600,800" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,400,500,700" rel="stylesheet">
+
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+
+
+<style>
+</style>
+</head>
+
+<body>
+<header>
+	<div class="header">
+		
+  	<div class="icons">
+
+  		<a class="fab fa-twitter icon" title="twitter" href="#"></a>
+    	<a class="fab fa-facebook-f icon" title="facebook" href="https://www.facebook.com/search/top/?q=west%20valley%20muslim%20association"></a>
+    	<a class="fab fa-youtube icon" title="youtube" href="https://www.youtube.com/watch?v=5qvp93COVLw"></a>
+    	<a class="far fa-clock icon" title="Prayer Times" href="#"></a>
+    </div>
+    <div class="logo"><img id="WVMA_logo" src="resources/arabesqueIcon3.png"></div>
+  	<div class="title"><a >WEST VALLEY MUSLIM ASSOCIATION</a></div>
+
+  <!-- navbar sit on top -->
+    <div class="menuLarge">
+      <a class="menu_item" href="#about" > ABOUT</a>
+      <a class="menu_item" href="#prayer" >PRAYER TIMES</a>
+      <a class="menu_item" href="https://sundayschool.wvmuslim.org/"> SUNDAY SCHOOL</a>
+      <a class="menu_item" href="#events" >EVENTS</a>
+      <a class="menu_item" href="#donate" > DONATE</a>
+      <a class="menu_item" href="#media" >MEDIA</a>
+      <a class="menu_item fas fa-search" href="#"></a>
+   	</div>
+
+  <!-- Navbar on small screens -->
+    <div class="menuSmall">
+      <div class="menu_icon dropdown"> 
+        <button type= "button" class="fa fa-bars menu_button" data-toggle="dropdown""></button>
+      
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#about">ABOUT</a>
+          <a class="dropdown-item " href="#prayer">PRAYER TIMES</a>
+          <a class="dropdown-item " href="https://sundayschool.wvmuslim.org/"> SUNDAY SCHOOL</a>
+          <a class="dropdown-item " href="#events" >EVENTS</a>
+          <a class="dropdown-item " href="#donate" > DONATE</a>
+          <a class="dropdown-item " href="#media" >MEDIA</a>
+          <a class="dropdown-item fas fa-search" href="#"></a>
+        </div>
+      </div>
+
+  </div>
+	
+</header>
+
+<div class="prayer_times">
+  <table class="prayer_times">
+  <tr>
+    <td class="row_title"></td>
+    <td class="prayer">Fajr</td> 
+    <td class="prayer">Dhuhr</td>
+    <td class="prayer">Asr</td> 
+    <td class="prayer">Maghreb</td>
+    <td class="prayer">Isha</td>
+  </tr>
+  <tr>
+    <td class="row_title">Jamaat</td>
+    <td class="jtime">6:00 AM</td> 
+    <td class="jtime">12:10 PM</td>
+    <td class="jtime">2:50 PM</td> 
+    <td class="jtime">5:30 PM</td>
+    <td class="jtime">8:00 PM</td>
+  </tr>
+  <tr>
+    <td class="row_title">Begins</td>
+    <td class="btime">6:00 AM </td> 
+    <td class="btime">12:10 PM</td>
+    <td class="btime">2:50 PM</td> 
+    <td class="btime">5:30 PM</td>
+    <td class="btime">8:00 PM</td>
+  </tr>
+
+</table>
+</div>
+
+
+<!-- carousel -->
+<div id="carousel">
+	<div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselIndicators" data-slide-to="3"></li>
+      <li data-target="#carouselIndicators" data-slide-to="4"></li>
+    </ol>
+  
+    <div class="carousel-inner" role="listbox">
+
+    <div class="carousel-item active">
+      <img class="carouselImg d-block img-fluid" src="resources/musalaTeal.jpg" alt="First slide">
+      <div class="carousel-caption">
+        <h3>SLIDE 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#" > some link to somewhere </a> Proin mollis ipsum eget dolor fringilla, id sollicitudin turpis tincidunt. Vestibulum ultricies at enim nec pretium. </p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img class="carouselImg d-block img-fluid" src="resources/ceilingYellow.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>SLIDE 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis ipsum eget dolor fringilla, id sollicitudin turpis tincidunt. Vestibulum ultricies at enim nec pretium. </p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img class="carouselImg d-block img-fluid" src="resources/samarkand.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>SLIDE 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis ipsum eget dolor fringilla, id sollicitudin turpis tincidunt. Vestibulum ultricies at enim nec pretium. </p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img class="carouselImg d-block img-fluid" src="resources/ceilingYellow2.jpg" alt="Fourth slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>SLIDE 4</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis ipsum eget dolor fringilla, id sollicitudin turpis tincidunt. Vestibulum ultricies at enim nec pretium. </p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img class="carouselImg d-block img-fluid" src="resources/istambul.jpg" alt="Fifth slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>SLIDE 5</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis ipsum eget dolor fringilla, id sollicitudin turpis tincidunt. Vestibulum ultricies at enim nec pretium. </p>
+      </div>
+    </div>
+
+   </div>  <!-- closes carousel-inner (images and captions)  -->
+
+
+    <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div> <!-- end of carousel -->
+
+
+<!-- First Parallax Image with Logo Text w3-xxlarge w3-hover-text-white w3-text-light-grey w3-wide  -->
+<div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+  <div class="mainTitle-display">
+    <span class="mainTitle ">West Valley Muslim Association</span>
+  </div>
+</div>
+
+<!-- Container (About Section) -->
+<div class="w3-content w3-container w3-padding-64" id="about">
+  <h3 class="w3-center">ABOUT WVMA</h3>
+  <!-- not sure if I'll ask permission  to post this... I might use some other motto -->
+      <p class="w3-center"> quote here</p>
+
+
+  
+  <div class="w3-row">
+    <div class="text" style="text-align: justify;">
+      <p>The West Valley Muslim Association serves offers religious services and community events to the Muslim communities in Saratoga, Sunnyvale, Campbell, and Cupertino. To become a member, please visit our Membership page. </p>
+      
+    </div>
+  </div>
+</div>
+
+<!-- Second Parallax Image with Portfolio Text -->
+<div class="bgimg-2 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+    <span class="w3-xxlarge w3-hover-text-white w3-text-black w3-wide ">DONATE</span>
+  </div>
+</div>
+
+<!-- Container (donate Section) -->
+<div class="w3-content w3-container w3-padding-64" id="donate">
+  
+  <p class="w3-center"><em>donate</em></p><br>
+
+  <!-- Responsive Grid. Four columns on tablets, laptops and desktops. Will stack on mobile devices/small screens (100% width)  -->
+  <div class="w3-row-padding w3-center">
+    <div class="w3-col m3">
+      <a href="SATproject.php"><img src="pix/SATvocabIcon.jpg" style="width:100%" class="w3-hover-opacity" alt="SAT Vocab" ></a>
+    </div>
+
+    <div class="w3-col m3">
+      <a href="Bubblesproject.php"><img src="pix/bubblesIcon2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Speculative Bubbles"></a>
+    </div>
+
+    <div class="w3-col m3">
+      <a href="MediaPlayerproject.php"><img src="pix/mediaPlayer3.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Media player"></a>
+    </div>
+
+    <div class="w3-col m3">
+      <a href="Dirsproject.php"><img src="pix/handlingDirs1.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Handling Directories"></a>
+    </div>
+  </div>
+
+  <div class="w3-row-padding w3-center w3-section">
+    <div class="w3-col m3">
+      <a href="Loginproject.php"><img src="pix/loginProj.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Login"></a>
+    </div>
+
+    <div class="w3-col m3">
+      <a href="Spiderproject.php"><img src="pix/spiderProj.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Spider News"></a>
+    </div>
+
+    <div class="w3-col m3">
+      <a href="Imageproject.php"><img src="pix/mlProj.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Image feature"></a>
+    </div>
+
+    <div class="w3-col m3">
+      <a href="WhatsNext.php"><img src="pix/whatsNext.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="whatsNext"></a>
+    </div>
+    <!-- <button class="w3-button w3-padding-large w3-light-grey" style="margin-top:64px">LOAD MORE</button> -->
+  </div>
+</div> 
+
+
+<!-- Third Parallax Image with Portfolio Text -->
+<div class="bgimg-3 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+    <span class="w3-xxlarge w3-hover-text-white w3-text-black w3-wide ">WORK</span>
+  </div>
+</div>
+
+</body>
+
+<!-- Footer -->
+<footer class="w3-center w3-light-white w3-padding-64 w3-opacity w3-hover-opacity-off">
+  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <a class="fa fa-github w3-button footer-social-media " style="font-size: 32px;" id="github" title="my github page" href="www.github.com/MarileneSoaresDaCosta" ></a>
+    <a class="fa fa-linkedin w3-button footer-social-media "  style="font-size: 32px;" id="linkedin" title="my linkedIn" href="www.linkedin.com/in/marilenesdacosta/"></a>
+    <a class="fa fa-file-pdf-o w3-button footer-social-media "  style="font-size: 32px;" id="pdf" title="Resume" href="pdf/msccv.pdf" ></a> 
+  </div>
+  
+</footer>
+
+
+
+<!-- last, before end of body: jQuery first, then Tether, then Bootstrap JS. -->
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
